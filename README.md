d7c72bd205a888f6ae5c90275784c3a52b582b4e
Max Shinn <max@maxshinnpotential.com>
2025-11-13 16:58:25 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,56 @@
+# Improved 3D segmentation for Cellpose
+
+## Summary
+
+When performing 3D segmentation on images without a very high signal-to-noise
+ratio, Cellpose can sometimes create striped or hash patterns, oversegmenting
+cells and making the segmentations unusable.  The following patch fixes this
+problem.
+
+## Usage
+
+Simply download this Python file and import it after after importing Cellpose:
+
+``` python
+import cellpose
+import cellpose3dplus
+```
+
+To confirm it is running, you will see "Running Cellpose3DPlus" printed to the
+command line when running 3D segmentation.
+
+If you run cellpose from the command line or use the GUI, you can alternatively
+use this in place of Cellpose.  Instead of:
+
+> python -m cellpose --Zstack
+
+Or:
+
+> cellpose --Zstack
+
+Use:
+
+> python -m cellpose3dplus --Zstack
+
+## Requirements
+
+This ONLY supports Cellpose version 3.1.1.
+
+## Technical details
+
+In brief, Cellpose's 3D algorithm makes three passes through the image: once on
+the z axis (running the segmentation network on each xy plane), once on the y
+axis (running it on each xz plane), and once on the x axis (running it on each
+yz plane).  The output of each run of the segmentation network is a map of the
+probability that a given location is a cell, and a gradient pointing towards the
+centre of the cell.  By default, Cellpose averages the cell probabilities for
+each run of the network with equal weighting.  However, usually, the z dimension
+(with xy planes) has a much stronger signal than the others.  This patch
+modifies cellpose's algorithm to use the cell probabilities and gradients
+derived from the most reliable run for all gradients except the z gradient, for
+which it averages this components of the y and x axis runs (discarding the y and
+x components and cell probabilities from these runs).
+
+<img align="right" src="https://raw.githubusercontent.com/mwshinn/cellpose3dplus/master/img/cellpose3dplus.jpg" width="100%">
+
+
---
+++ cellpose3dplus.py
@@ -0,0 +1,74 @@
+# Patch by Max shinn 2025: https://github.com/mwshinn/cellpose3dplus
+# Available under the MIT license
+import numpy as np
+from cellpose.core import core_logger, run_net
+import cellpose.models
+from packaging.version import Version
+
+v = Version(cellpose.version)
+assert v.major == 3 and v.minor == 1, "This package requires cellpose version 3.1.x (ideally 3.1.1.2)"
+if v != Version("3.1.1.2"):
+    print("Warning, this patch was only tested on Cellpose 3.1.1.2, use at your own risk!")
+
+def run_3D_xy_zsplit(net, imgs, batch_size=8, augment=False,
+                     tile_overlap=0.1, bsize=224, net_ortho=None,
+                     progress=None):
+    """
+    Monkey patch for cellpose.core.run_3D
+
+    Get X and Y flows and cellprob from XY planes, and Z flows from YZ and XZ planes.
+    """
+    core_logger.info("Running Cellpose3DPlus")
+    sstr = ["YX", "ZY", "ZX"]
+    pm   = [(0, 1, 2, 3), # XY
+            (1, 0, 2, 3), # YZ
+            (2, 0, 1, 3)] # XZ
+    ipm  = [(0, 1, 2), (1, 0, 2), (1, 2, 0)]
+    
+    shape = imgs.shape[:-1] # Z,Y,X
+    yf    = np.zeros((*shape, 4), np.float32) # Z, Y, X, cell-prob
+    zflow_sum = np.zeros(shape, np.float32)
+    
+    # First run in the xy dimension
+    core_logger.info("running %s: %d planes of size (%d, %d)",
+                     sstr[0], shape[0], shape[1], shape[2])
+    y_xy, style = run_net(net, imgs,
+                          batch_size=batch_size, augment=augment,
+                          bsize=bsize, tile_overlap=tile_overlap, rsz=None)
+    # channel-map: 0 = Y flow, 1 = X flow, 2 = cellprob
+    yf[..., 1] = y_xy[..., 0]
+    yf[..., 2] = y_xy[..., 1]
+    yf[..., 3] = y_xy[..., 2]
+    if progress is not None:
+        progress.setValue(25)
+        
+    # Now run in xy and yz dimensions
+    for p in (1, 2):
+        xsl = imgs.transpose(pm[p])
+        core_logger.info("running %s: %d planes of size (%d, %d)",
+                         sstr[p], shape[pm[p][0]],
+                         shape[pm[p][1]], shape[pm[p][2]])
+        y_ortho, _ = run_net(net if net_ortho is None else net_ortho,
+                             xsl, batch_size=batch_size, augment=augment,
+                             bsize=bsize, tile_overlap=tile_overlap, rsz=None)
+        # channel 0 of orthogonal output is the Z flow for that view
+        zflow_sum += y_ortho[..., 0].transpose(ipm[p])
+        if progress is not None:
+            progress.setValue(25 + 35 * p)
+        del y_ortho
+    # average the two orthogonal estimates
+    yf[..., 0] = zflow_sum / 2.0
+    if progress is not None:
+        progress.setValue(95)
+    return yf, style
+
+
+cellpose.core.run_3D = run_3D_xy_zsplit
+cellpose.run_3D = run_3D_xy_zsplit
+cellpose.models.run_3D = run_3D_xy_zsplit
+core_logger.info("Patched cellpose.core.run_3D with Cellpose3DPlus")
+
+# Allow running from the command line
+if __name__ == "__main__":
+    import cellpose.__main__
+    cellpose.__main__.main()
